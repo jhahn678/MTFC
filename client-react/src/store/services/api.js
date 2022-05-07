18d6b14568a2b287aee0fe56f2256d0dadeb286c
jhahn678 <jhahn678@yahoo.com>
2022-05-07 11:21:03 -0400

    added api url to client

--- client-react/src/store/services/api.js
+++ client-react/src/store/services/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3000/',
+        baseUrl: `${process.env.REACT_APP_API_URL}`,
         prepareHeaders: (headers, { getState }) => {
             const token = getState().user.token;
             if(token){
