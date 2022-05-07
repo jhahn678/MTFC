8675b821d866e5e5f65ea8ff606b83b0e3a94fdd
jhahn678 <jhahn678@yahoo.com>
2022-05-07 13:09:00 -0400

    added netlify redirects file

--- client-react/src/components/UI/forms/LoginForm/LoginForm.js
+++ client-react/src/components/UI/forms/LoginForm/LoginForm.js
@@ -96,7 +96,7 @@ const LoginForm = ({ className, onDismiss, onRedirect, handleCartMerge }) => {
                 toast.error('Could not authenticate')
             }
         }else{
-            console.log(res.error)
+            console.log(res)
             toast.error('Cannot connect to Google')
         }
     }
