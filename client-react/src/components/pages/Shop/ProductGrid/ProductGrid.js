4606dc1fc62b7750366436d6017e3877a01affb3
jhahn678 <jhahn678@yahoo.com>
2022-05-07 22:57:52 -0400

    Adding loading animation for shop product grid

--- client-react/src/components/pages/Shop/ProductGrid/ProductGrid.js
+++ client-react/src/components/pages/Shop/ProductGrid/ProductGrid.js
@@ -1,6 +1,31 @@
 import classes from './ProductGrid.module.css'
 import ProductCard from '../ProductCard/ProductCard'
 import { motion, AnimatePresence } from 'framer-motion'
+import CircularProgress from '@mui/material/CircularProgress';
+
+const headerVariants = {
+  initial: {},
+  loading: {
+    transition: {
+      delayChildren: .3,
+      staggerChildren: .2,
+      repeat: Infinity
+    }
+  }
+}
+
+const letterVariants = {
+  initial: {
+    y: 0
+  },
+  loading: {
+    y: [0, -15, 0, 0, 0, 0],
+    transition: {
+      duration: 2,
+      repeat: Infinity
+    }
+  }
+}
 
 const ProductGrid = ({ data, isLoading, isSuccess, isError }) => {
   return (
@@ -18,13 +43,24 @@ const ProductGrid = ({ data, isLoading, isSuccess, isError }) => {
       {/* Handle fetch states */}
       {
         isSuccess && data.products.length < 1 && 
-        <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1}}>No products matched</motion.h1>
+        <h1>No products matched</h1>
       }
       {
-        isLoading && <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1}}>Loading</motion.h1>
+        isLoading && 
+        <motion.h1 style={{ display: 'flex', marginLeft: '3vw', marginTop: '3vh', fontSize: '3em', color: 'var(--text)' }}
+          variants={headerVariants} initial='initial' animate='loading'
+        >
+          <motion.span variants={letterVariants}>L</motion.span>
+          <motion.span variants={letterVariants}>o</motion.span>
+          <motion.span variants={letterVariants}>a</motion.span>
+          <motion.span variants={letterVariants}>d</motion.span>
+          <motion.span variants={letterVariants}>i</motion.span>
+          <motion.span variants={letterVariants}>n</motion.span>
+          <motion.span variants={letterVariants}>g</motion.span>
+        </motion.h1>
       }
       {
-        isError && <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1}}>Error</motion.h1>
+        isError && <h1>Error loading products. Please refresh browser</h1>
       }
     </main>
   )
