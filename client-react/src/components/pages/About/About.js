cc4c30eab9c698bce1ff32bb08b3c5515a1ca2b3
jhahn678 <jhahn678@yahoo.com>
2022-10-14 15:14:57 -0400

    About page

--- client-react/src/components/pages/About/About.js
+++ client-react/src/components/pages/About/About.js
@@ -1,10 +1,31 @@
 import Page from '../../UI/Page/Page'
 import classes from './About.module.css'
+import { motion } from 'framer-motion'
 
 const About = () => {
   return (
     <Page className={classes.aboutPage}>
-      <h1>Coming soon..</h1>
+      <div className={classes.container}>
+        <div className={classes.content}>
+          <motion.h1 
+            initial={{ opacity: 0 }} 
+            animate={{ opacity: 1 }} 
+            viewport={{ once: true }} 
+            className={classes.heading}
+          >
+            About
+          </motion.h1>
+          <motion.p 
+            initial={{ opacity: 0 }} 
+            animate={{ opacity: 1 }} 
+            viewport={{ once: true }} 
+            className={classes.text}
+          >
+            More information about the shop coming soon!
+            Check out our Facbook Page in the mean time to see more
+          </motion.p>
+        </div>
+      </div>
     </Page>
   )
 }
